b3870f2d3acf757889694a8153c97374d20463c1
Joshua Hoblitt <josh@hoblitt.com>
2015-06-15 11:23:09 -0700

    update to puppetlabs-apt 2.x API

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,23 +2,18 @@ class hipchat::repo {
 
   case $::osfamily {
     'Debian': {
-
       apt::source { 'hipchat':
         location    => 'http://downloads.hipchat.com/linux/apt',
         release     => 'stable',
         repos       => 'main',
         include_src => false,
+        key         => {
+          source => 'https://www.hipchat.com/keys/hipchat-linux.key',
+        },
       }
-
-      apt::key { 'hipchat':
-        key_source => 'https://www.hipchat.com/keys/hipchat-linux.key',
-      }
-
     }
     default: {
       fail("${::osfamily} is not supported")
     }
-
   }
-
 }
